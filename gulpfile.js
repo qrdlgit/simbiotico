66a89c9b343c98426d94b43ce0c83fe91c020aae
tim <tim@tim.com>
2023-04-14 17:25:04 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { src, dest, series, watch } = require('gulp');
+const concat = require('gulp-concat');
+const inlineSource = require('gulp-inline-source');
+const fileInclude = require('gulp-file-include');
+
+function html() {
+    return src('src/*.html')
+        .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
+        .pipe(inlineSource())
+        .pipe(dest('dist'));
+}
+
+function css() {
+    return src('src/css/*.css')
+        .pipe(concat('styles.css'))
+        .pipe(dest('src'));
+}
+
+function js() {
+    return src('src/js/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(dest('src'));
+}
+
+function watchFiles() {
+    watch('src/*.html', series(html));
+    watch('src/partials/*.html', series(html));
+    watch('src/css/*.css', series(css));
+    watch('src/js/*.js', series(js));
+}
+
+exports.default = series(html, css, js, watchFiles);
+
+
